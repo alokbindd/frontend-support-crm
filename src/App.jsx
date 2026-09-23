65b869c8a2b50fc546602d9e27821bd20c6f5bb2
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-09-23 14:48:55 +0530

    Add TicketDetails component for enhanced ticket viewing experience

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import API_BASE_URL from "./config/api";
 import { useEffect, useState } from "react";
 import TicketCard from "./components/TicketCard";
 import CreateTicketForm from "./components/CreateTicketForm";
+import TicketDetails from "./components/TicketDetails";
 
 function App() {
   const [tickets, SetTickets] = useState([]);
   const [loading, setLoading] = useState(true);
   const [search, setSearch] = useState("");
   const [status, setStatus] = useState("All");
+  const [selectedTicketId, setSelectedTicketId] = useState(null);
 
   const fetchTickets = async () => {
     const response = await fetch(`${API_BASE_URL}/api/tickets`);
@@ -33,33 +35,46 @@ function App() {
 
   return (
     <div>
-      <h1>Support CRM</h1>
-
-      <CreateTicketForm onTicketCreated={fetchTickets} />
-
-      <input
-        type="text"
-        placeholder="Search Ticket..."
-        value={search}
-        onChange={(event) => setSearch(event.target.value)}
-      />
-
-      <select
-        value={status}
-        onChange={(event) => setStatus(event.target.value)}
-      >
-        <option value="All">All</option>
-        <option value="Open">Open</option>
-        <option value="In Progress">In Progress</option>
-        <option value="Closed">Closed</option>
-      </select>
-
-      {loading ? (
-        <p>Loading tickets...</p>
+      {selectedTicketId ? (
+        <TicketDetails
+          ticketId={selectedTicketId}
+          onBack={() => setSelectedTicketId(null)}
+        />
       ) : (
-        filteredTicket.map((ticket) => (
-          <TicketCard key={ticket.ticket_id} ticket={ticket} />
-        ))
+        <>
+          <h1>Support CRM</h1>
+
+          <CreateTicketForm onTicketCreated={fetchTickets} />
+
+          <input
+            type="text"
+            placeholder="Search Ticket..."
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
+          />
+
+          <select
+            value={status}
+            onChange={(event) => setStatus(event.target.value)}
+          >
+            <option value="All">All</option>
+            <option value="Open">Open</option>
+            <option value="In Progress">In Progress</option>
+            <option value="Closed">Closed</option>
+          </select>
+
+          {loading ? (
+            <p>Loading tickets...</p>
+          ) : (
+            filteredTicket.map((ticket) => (
+              <TicketCard
+                key={ticket.ticket_id}
+                ticket={ticket}
+                onClick={() => setSelectedTicketId(ticket.ticket_id)}
+              />
+            ))
+          )}
+        </>
       )}
     </div>
   );
